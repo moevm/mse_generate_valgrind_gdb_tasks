94e02b91016676c7fb9b404a6985e983edc6467b
MariyaF1998 <mask-1998@mail.ru>
2017-12-20 17:07:18 +0400

    Obfuscated code for gdb task sorting

---
+++ obfus-sort.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define ReplacementFor_MAX_SIZE (0xfdf+4867-0x227e)
+int main(){int array[ReplacementFor_MAX_SIZE];int ReplacementFor_size;int i,
+ReplacementFor_j,ReplacementFor_temp;printf(
+"\x45\x6e\x74\x65\x72\x20\x73\x69\x7a\x65\x20\x6f\x66\x20\x61\x72\x72\x61\x79\x3a\x20"
+);scanf("\x25\x64",&ReplacementFor_size);printf(
+"\x45\x6e\x74\x65\x72\x20\x65\x6c\x65\x6d\x65\x6e\x74\x73\x20\x69\x6e\x20\x61\x72\x72\x61\x79\x3a\x20"
+);for(i=(0xea2+5237-0x2317);i<ReplacementFor_size;i++){scanf("\x25\x64",&array[i
+]);}for(i=(0x4af+714-0x779);i<ReplacementFor_size;i++){for(ReplacementFor_j=i+
+(0xbf6+1324-0x1121);ReplacementFor_j<ReplacementFor_size;ReplacementFor_j++){if(
+array[i]>array[ReplacementFor_j]){ReplacementFor_temp=array[i];array[
+ReplacementFor_j]=array[i];array[ReplacementFor_j]=ReplacementFor_temp;}}}printf
+(
+"\n" "\x45\x6c\x65\x6d\x65\x6e\x74\x73\x20\x6f\x66\x20\x74\x68\x65\x20\x73\x6f\x72\x74\x65\x64\x20\x61\x72\x72\x61\x79\x3a\x20"
+);for(i=(0x1abd+1856-0x21fd);i<ReplacementFor_size;i++){printf("\x25\x64" "\t",
+array[i]);}return(0x18b2+2136-0x210a);}
